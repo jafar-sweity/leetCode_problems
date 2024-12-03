6aab7a4a6e4227662a47d73ed17cf8fb80086207
jafar-sweity <124098267+jafar-sweity@users.noreply.github.com>
2024-12-03 21:16:38 +0200

    Time: 1 ms (45.19%) | Memory: 41.1 MB (21.49%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
 
-        for (int i =0 ; i <=31; i++)
+        for (int i =0 ; i <=30; i++)
         {
             if (Math.pow(2,i)==n)
             return true;
