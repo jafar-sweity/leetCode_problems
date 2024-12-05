a6ef5c0107cd0f58cc233dd97dcc9d5ba7b099d4
jafar-sweity <124098267+jafar-sweity@users.noreply.github.com>
2024-12-05 14:51:42 +0200

    Time: 1 ms (99.85%) | Memory: 46.5 MB (7.77%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -4,16 +4,9 @@ class Solution {
         int x =0;
         for (int i =0;i<nums.length ;i++)
         {
-            mymap.put(nums[i],mymap.getOrDefault(nums[i],0)+1);
+            x  = x ^ nums[i];
         }
-         for (int i =0;i<nums.length ;i++)
-        {
-           if (mymap.get(nums[i])==1)
-           {
-             x= nums[i];
-           }
-          
-        }
+         
 
         return x;
 
